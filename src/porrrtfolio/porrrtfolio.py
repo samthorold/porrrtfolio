9f00b51288c1d33679218e89bcfed67699cfd8c4
Sam Thorold <sam.thorold@gmail.com>
2023-01-07 17:32:13 +0000

    Store data on fetch

--- src/porrrtfolio/porrrtfolio.py
+++ src/porrrtfolio/porrrtfolio.py
@@ -1,18 +1,32 @@
 """Porrrtfolio analysis."""
 
+import json
 import os
+import pathlib
 
 import requests
 
 
 _BASE_URL = "https://www.alphavantage.co/query"
+_DATA_DIR = pathlib.Path("data/av")
 
 
-def daily_prices(symbol: str, exchange: str | None = None, key: str | None = None) -> dict:
-    key = os.getenv("AV_KEY") if key is None else key
+def prices(
+    symbol: str,
+    exchange: str | None = None,
+    read_from_cache: bool = True,
+    key: str | None = None
+) -> dict:
+    _DATA_DIR.mkdir(parents=True, exist_ok=True)
     if exchange:
         symbol = f"{exchange}.{symbol}"
-    return requests.get(
+    path = pathlib.Path(f"{_DATA_DIR / symbol}.json")
+    if read_from_cache:
+        if path.exists():
+            with open(path) as f:
+                return json.load(f)
+    key = os.getenv("AV_KEY") if key is None else key
+    d = requests.get(
         url=_BASE_URL,
         params={
             "function": "TIME_SERIES_DAILY_ADJUSTED",
@@ -21,3 +35,7 @@ def daily_prices(symbol: str, exchange: str | None = None, key: str | None = Non
             "apikey": key,
         }
     ).json()
+    if read_from_cache:
+        with open(path, "w") as f:
+            json.dump(d, f)
+    return d
